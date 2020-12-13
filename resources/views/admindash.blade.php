1112f435b8d6c4064ab7ebd4dfae10748d24a01f
james fu <fuzihaoooo@gmail.com>
2020-12-14 00:42:24 +1100

    changes to nav bar, added some buttons in quote dash and admin dash

--- resources/views/admindash.blade.php
+++ resources/views/admindash.blade.php
@@ -4,6 +4,10 @@
 
 @section('content')
 <!-- --------------- -->
+<style>
+    
+</style>
+
 <html>
 
 <head>
@@ -12,7 +16,34 @@
 
 <body>
 
-<h3> Admin Dashboard </h3>
+    <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <a href="/{{ 'totalcosts' }}" class="list-group-item list-group-item-action bg-light border-0">Total Cost</a>
+          </div>
+          <div class="col-sm">
+            <a href="/{{ 'employeecosts' }}" class="list-group-item list-group-item-action bg-light border-0">employeecosts</a>
+          </div>
+          <div class="col-sm">
+            <a href="/{{ 'companycosts' }}" class="list-group-item list-group-item-action bg-light border-0">companycosts</a>
+          </div>
+          <div class="col-sm">
+            <a href="/{{ 'discounts' }}" class="list-group-item list-group-item-action bg-light border-0">discounts</a>
+          </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm">
+              <a href="/{{ 'users' }}" class="list-group-item list-group-item-action bg-light border-0">User management</a>
+            </div>
+            <div class="col-sm">
+              <a href="/{{ 'adminqdash' }}" class="list-group-item list-group-item-action bg-light border-0">Quote Managment</a>
+            </div>
+            <div class="col-sm">
+              <a href="/{{ 'businessdetails' }}" class="list-group-item list-group-item-action bg-light border-0">Business Datail</a>
+            </div>
+          </div>
+      </div>
 
 </body>
 
