ebc41c9efd0d61ce1e0531f0d93b840a1312b97b
Johnny Tran <19005140@student.westernsydney.edu.au>
2020-12-15 22:48:17 +1100

    More Price list changes -_-

--- resources/views/pricelist.blade.php
+++ resources/views/pricelist.blade.php
@@ -32,7 +32,7 @@
 <!-- Button trigger modal -->
 <div class=" p-3 bg-white rounded border">
 <h3> Price List <a type="button" class="p-2 btn btn-outline-primary" href="/{{ 'categories' }}"> Category Management </h3></a> </h3> 
-        <div class="p-4 form-row row-cols-5">
+        <div class="py-5 form-row row-cols-4 d-flex justify-content-center">
         <x-sidebarCategories/>   
         </div>
         
