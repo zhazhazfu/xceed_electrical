d52b99930eafe174ba46e8096ee2fea346e5e4b6
Johnny Tran <19005140@student.westernsydney.edu.au>
2020-12-15 21:45:32 +1100

    made a .png for Logo and minor changes to Price List

--- resources/views/components/sidebar-categories.blade.php
+++ resources/views/components/sidebar-categories.blade.php
@@ -1,6 +1,7 @@
 
 @foreach($categories as $category)
     @if($category->category_archived == '0')
-    <a href="{{action('PriceListController@show', $category['pk_category_id'])}}" class="p-5 m-1 btn btn-outline-primary" id="categories" value="{{ $category->pk_category_id }}"> <h3 class="float-right"> {{ $category->category_name }} </h3></a>
+    <a href="{{action('PriceListController@show', $category['pk_category_id'])}}" class="p-5 m-1 btn btn-outline-primary" 
+    id="categories" value="{{ $category->pk_category_id }}"> <h3 class="float-right"> {{ $category->category_name }} </h3></a>
     @endif
 @endforeach
--- resources/views/pricelist.blade.php
+++ resources/views/pricelist.blade.php
@@ -16,7 +16,6 @@
 <div class=" p-3 bg-white rounded border">
 <h3> Price List <a type="button" class="p-2 btn btn-outline-primary" href="/{{ 'categories' }}"> Category Management </h3></a> </h3> 
         <div class="p-4 form-row row-cols-4">
-            
         <x-sidebarCategories/>   
         </div>
         
