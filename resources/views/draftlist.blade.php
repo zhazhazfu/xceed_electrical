3f7b92ca8d8c3b13df34750dee42cef436b4e8ea
ahmerjaved994 <73347171+ahmerjaved994@users.noreply.github.com>
2020-12-15 21:42:33 +1100

    update draft

--- resources/views/draftlist.blade.php
+++ resources/views/draftlist.blade.php
@@ -11,23 +11,57 @@
 </head>
 
 <body>
-
 <h3> Draft list </h3>
-<div class="container"> <!-- i will add css for table design later -->
-  <div class="row">
-    <div class="col-sm-3">Quote ID</div>
-    <div class="col-sm-3">Quote Date</div>
-    <div class="col-sm-3">Client Name</div>
-    <div class="col-sm-3">Description</div>
-  </div>
-  <div class="row">
-    <div class="col-*-*">1846543</div> <!-- testing row -->
-    <div class="col-*-*"></div>
-    <div class="col-*-*"></div>
-  </div>
-  <div class="row">
-  </div>
-</div>
+<table class="table table-hover">
+<thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Quote ID</th>
+      <th scope="col">Quote Date</th>
+      <th scope="col">Client Name</th>
+      <th scope="col">Description</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+
+      <th scope="row">1</th>
+      <td>1846453</td>
+      <td>12/12/2020</td>
+      <td>James Anderson</td>
+      <td>Lightning</td>
+      <td >
+      <button type="button" class="btn btn-primary badge-pill" style="width:80px;">Edit</button>
+      <button type="button" class="btn btn-danger badge-pill"style="width:80px;">Delete</button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>1846454</td>
+      <td>13/12/2020</td>
+      <td>Johnny Thornton</td>
+      <td>CCTV</td>
+      <td >
+      <button type="button" class="btn btn-primary badge-pill" style="width:80px;">Edit</button>
+      <button type="button" class="btn btn-danger badge-pill"style="width:80px;">Delete</button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>1846455</td>
+      <td>13/12/2020</td>
+      <td>Sammy John</td>
+      <td>Alarms</td>
+      <td >
+      <button type="button" class="btn btn-primary badge-pill" style="width:80px;">Edit</button>
+      <button type="button" class="btn btn-danger badge-pill"style="width:80px;">Delete</button>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+
+
 </body>
 
 </html>
