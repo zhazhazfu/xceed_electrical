5ec39c92a903f7e8448bd029fba94c368502c0c5
james fu <fuzihaoooo@gmail.com>
2020-12-16 18:50:24 +1100

    user authrisation added in quote dash

--- resources/views/qdash.blade.php
+++ resources/views/qdash.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
 <!-- --------------- -->
+@if (Auth::user())
 <html>
 
 <head>
@@ -22,28 +23,55 @@ h1 {
 </style>
 
 <body>
+    {{-- <div class="row">
+        <div class="col-sm">
+            <a href="/{{ 'history' }}" class="list-group-item list-group-item-action bg-light border-0">History</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm">
+                <a href="/{{ 'draftlist' }}" class="list-group-item list-group-item-action bg-light border-0">Draft</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm">
+            <a href="/{{ 'quoting' }}" class="list-group-item list-group-item-action bg-light border-0">Fix Quote</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm">
+            <a href="/{{ 'perpointquote' }}" class="list-group-item list-group-item-action bg-light border-0">Per point quote</a>
+        </div>
+    </div> --}}
+
+
 <h1 style = "text-align: center;" class = "display-3">Quote Dashboard</h1>
     <div class="container text-center">
     <div class = "row" style = "padding-left: 180px;">
         <div class="col-xs-5 col-md-5">
-        <div class="box1">
+        <div class="box1 m-1">
                 <a href="/{{ 'history' }}" class="list-group-item list-group-item-action flex-column shadow-lg rounded display-4" style = "height: 200px;">History</a>
         </div>
-        <div class="box2">
-                <a href="/{{ 'draftlist' }}" class="list-group-item list-group-item-action flex-column shadow-lg rounded display-4" style = "height: 200px;">Draft</a>
+        <div class="box2 m-1">
+                
+                <a href="/{{ 'quoting' }}" class="list-group-item list-group-item-action flex-column shadow-lg rounded display-4" style = "height: 200px">Fix Quote</a>
         </div>
         </div>
         <div class="col-xs-5 col-md-5" style = "margin-left: -30px;">
-        <div class="box3">
-                <a href="/{{ 'quoting' }}" class="list-group-item list-group-item-action flex-column shadow-lg rounded display-4" style = "height: 200px">Fix Quote</a>
+        <div class="box3 m-1">
+            <a href="/{{ 'draftlist' }}" class="list-group-item list-group-item-action flex-column shadow-lg rounded display-4" style = "height: 200px;">Draft</a>
         </div>
-        <div class="box4">
-                <a href="/{{ 'perpointquote' }}" class="list-group-item list-group-item-action flex-column shadow-lg rounded display-4" style = "height: 200px">Per point quote</a>
+        @if (Auth::user() && Auth::user()->role == 'admin')
+        <div class="box4 m-1">
+                <a href="/{{ 'perpointquote' }}" class="list-group-item list-group-item-action flex-column shadow-lg rounded display-4" style = "height: 200px">Per Point Quote</a>
         </div>
+
         </div> 
     </div>
+    @endif
     </div>
 </body>
 
 </html>
+@endif
 @endsection
