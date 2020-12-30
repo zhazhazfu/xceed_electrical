ec500801b9134154452a6c926ebb4018c3b04a72
james fu <fuzihaoooo@gmail.com>
2020-12-31 00:04:32 +1100

    migration add for item category and add table subcategory

--- database/migrations/2020_12_29_005913_add_user.php
+++ database/migrations/2020_12_29_005913_add_user.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Hash;
 
-class AddUserTypeToTableUsers extends Migration
+class AddUser extends Migration
 {
     /**
      * Run the migrations.
