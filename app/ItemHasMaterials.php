e52ee9691998c2f18f37d356d77e2dd6348e9408
james fu <fuzihaoooo@gmail.com>
2021-01-03 19:37:36 +1100

    item model and ihm model changes

--- app/ItemHasMaterials.php
+++ app/ItemHasMaterials.php
@@ -12,14 +12,17 @@ class ItemHasMaterials extends Model
             'fk_item_id', 
             'fk_material_id'
         ];
+
+    public function items()
+    {
+        return $this->belongsTo('App\Items', 'fk_item_id', 'pk_item_id');
+    }
+
+    public function material()
+    {
+        return $this->belongsTo('App\Material', 'fk_material_id', 'pk_material_id');
+    }
 }
 
 
-// Relationships to be added:
 
-// One-to-Many:
-// QuoteItemMaterial
-
-// Many-to-One:
-// Quote
-// Product
