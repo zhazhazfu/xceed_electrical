d23be9a3238276b3e5607c45d11c171cbb008cf3
Johnny Tran <19005140@student.westernsydney.edu.au>
2021-01-09 13:25:30 +1100

    Price List icons fix

--- resources/views/components/sidebar-categories.blade.php
+++ resources/views/components/sidebar-categories.blade.php
@@ -2,10 +2,18 @@
 @foreach($categories as $category)
     @if($category->category_archived == '0')
     <a href="{{action('PriceListController@show', $category['pk_category_id'])}}" class="p-4 m-2 btn btn-outline-xceed"
-    id="categories" value="{{ $category->pk_category_id }}"> <div class='tint'></div> 
-    <svg> 
-        <path d="{{ $category->category_icon }}" fill="#004271"/>
-    </svg>
-    <h3 class="float-right"> {{ $category->category_name }} </h3></a>
+        id="categories" value="{{ $category->pk_category_id }}">
+            <svg
+            height="100px"
+            viewBox="0 0 65 65"
+            version="1.1"
+            class=" h-auto w-auto float-left">
+                <g>
+                    <path
+                    d="{{ $category->category_icon }}" fill="#004271"/>
+                </g>
+            </svg>
+        <h3 class="float-right"> {{ $category->category_name }} </h3>
+    </a>
     @endif
 @endforeach
