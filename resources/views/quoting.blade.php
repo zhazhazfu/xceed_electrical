8aae824b074b260b405042110d7ebfb49627eced
Johnny Tran <19005140@student.westernsydney.edu.au>
2021-01-10 19:56:29 +1100

    Add Row Function

--- resources/views/quoting.blade.php
+++ resources/views/quoting.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
-@section('title', 'Per Point Quoting')
+@section('title', 'Quoting')
 
 @section('content')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
 <div class="container rounded border pl-5 pr-5 pb-5">
     <h2 class="mt-3 mb-4">Create Quote</h2>
@@ -58,48 +59,77 @@
             </div>
         </div>
 
-        <div class="w-100 border-top"></div>
+
         <div class="col-sm-12 pb-2">
             <h5 class="pt-3 pb-1">Job</h5>
-            <div class="form-row">
-                <div class="form-group">
-                </div>
-                <div class="form-group col-md-1">
-                    <label for="itemNo">#</label>
-                    <input type="text" class="form-control" id="itemNo" placeholder="#" readonly>
-                </div>
-                <div class="form-group col-md-4">
-                    <label for="selectCategory">Category</label>
-                    <select class="form-control" id="selectCategory">
-                        @foreach($categories as $category)
-                        @if($category->category_archived == '0')
-                        <option value="{{ $category->pk_category_id }}">{{ $category->category_name }}</option>
-                        @endif
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group col-md-4">
-                    <label for="selectCategory">Sub-Category</label>
-                    <select class="form-control" id="fk_subcategory_id" name="fk_subcategory_id">
-                        @foreach($subCategories as $subCategory)
-                        <option value="{{ $subCategory -> pk_subcategory_id }}">
-                            {{ $subCategory -> subcategory_name }}
-                        </option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group col-md-3">
-                    <label for="selectItemNumber">Item Code</label>
-                    <select class="form-control" id="item_number" name="item_number">
-                        @foreach($priceLists as $priceList)
-                        @if($priceList->item_archived == '0')
-                        <option value="{{ $priceList->pk_item_id }}">{{ $priceList->item_number }}</option>
-                        @endif
-                        @endforeach
-                    </select>
+
+            <!-- <button onclick="cloneJob()" class="btn btn-outline-secondary"> Add another job </button>
+            <br> <br>
+            @php
+                $counter = 1;
+            @endphp
+
+            @for ($x=1; $x<=$counter; $x++) -->
+
+            <div id="job" class="form-row">
+                <table class="table" id="items_table">
+                    <tbody>
+                        <tr id="item0" class="form-group">
+
+                            <td>
+                                <label for="itemNo">#</label>
+                                <input type="text" class="form-control" id="itemNo" placeholder="#" readonly>
+                            </td>
+
+                            <td>
+                                <label for="selectCategory">Category</label>
+                                <select class="form-control" id="selectCategory">
+                                    @foreach($categories as $category)
+                                    @if($category->category_archived == '0')
+                                    <option value="{{ $category->pk_category_id }}">{{ $category->category_name }}</option>
+                                    @endif
+                                    @endforeach
+                                </select>
+                            </td>
+
+                            <td>
+                                <label for="selectCategory">Sub-Category</label>
+                                <select class="form-control" id="fk_subcategory_id" name="fk_subcategory_id">
+                                    @foreach($subCategories as $subCategory)
+                                    <option value="{{ $subCategory -> pk_subcategory_id }}">
+                                        {{ $subCategory -> subcategory_name }}
+                                    </option>
+                                    @endforeach
+                                </select>
+                            </td>
+
+                            <td width="15%">
+                                <label for="selectItemNumber">Item Code</label>
+                                <select class="form-control" id="item_number" name="item_number">
+                                    @foreach($priceLists as $priceList)
+                                    @if($priceList->item_archived == '0')
+                                    <option value="{{ $priceList->pk_item_id }}">{{ $priceList->item_number }}</option>
+                                    @endif
+                                    @endforeach
+                                </select>
+                        </tr>
+
+                        <tr id="item1"></tr>
+                    </tbody>
+                </table>
+            </div>
+            
+
+            <div class="row">
+                <div class="col-md-12">
+                    <button id="add_row" class="btn btn-outline-primary float-left">+ Add Row</button>
+                    <button id='delete_row' class="btn btn-outline-danger float-right">- Delete Row</button>
                 </div>
             </div>
+
+            @endfor
         </div>
+        
 
         <div class="w-100 border-top"></div>
         <div class="col-sm-12 pb-2">
@@ -191,8 +221,40 @@
 </div>
 
 <script>
+    counter = 1;
     let fk_subcategory_id = $("#my_select").change(function () {
         var id = $(this).children(":selected").attr("id");
+    });
+
+    function cloneJob() {
+        counter++;
+        alert('Counter = ' + counter);
+    }
+
+  $(document).ready(function(){
+        let row_number = 1;
+
+        $("#add_row").click(function(e){
+
+            e.preventDefault();
+
+            let new_row_number = row_number - 1;
+            $('#item' + row_number).html($('#item' + new_row_number).html()).find('td:first-child');
+
+            $('#items_table').append('<tr id="item' + (row_number + 1) + '"></tr>');
+            row_number++;
+
+        });
+
+        $("#delete_row").click(function(e){
+            alert('please work ;_;');
+            e.preventDefault();
+
+            if(row_number > 1){
+                $("#item" + (row_number - 1)).html('');
+                row_number--;
+        }
+    });
     });
 
 </script>
