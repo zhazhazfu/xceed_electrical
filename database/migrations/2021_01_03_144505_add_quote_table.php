a90055868714e87fde1b9360d0f3b56c091d3899
james fu <fuzihaoooo@gmail.com>
2021-01-30 12:31:03 +1100

    in and ex type change to longtext

--- database/migrations/2021_01_03_144505_add_quote_table.php
+++ database/migrations/2021_01_03_144505_add_quote_table.php
@@ -23,8 +23,8 @@ class AddQuoteTable extends Migration
             // $table->foreignId('fk_ex_id')->references('pk_ex_id')->on('exclusions');
             $table->foreignId('fk_prefix_id')->references('pk_prefix_id')->on('prefixes');
             $table->string('quote_number');
-            $table->string('inclusions');
-            $table->string('exclusions');
+            $table->longText('inclusions');
+            $table->longText('exclusions');
             $table->integer('quote_status')->default(1);
             $table->integer('quote_revisonnumber')->nullable();
             $table->string('quote_comment')->nullable();
