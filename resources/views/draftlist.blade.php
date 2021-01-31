8f07176cfa2b999b63c4150f758edeabbde49cc0
james fu <fuzihaoooo@gmail.com>
2021-02-01 00:21:08 +1100

    small draft table change

--- resources/views/draftlist.blade.php
+++ resources/views/draftlist.blade.php
@@ -21,7 +21,6 @@
                       <th scope="col">Client Name</th>
                       <th scope="col">Comment</th>
                       <th scope="col"></th>
-                      <th scope="col"></th>
                   </tr>
               </thead>
               <tbody>
@@ -31,7 +30,6 @@
                             <td>{{$quote->created_at}}</td>
                             <td>{{$quote->customers->customer_name}}</td>
                             <td>{{$quote->quote_comment}}</td>
-                            <td>{{$quote->type}}</td>
                             <td>
                             <a href="{{url('/draftlist/edit/'.$quote->pk_quote_id )}}" class="btn btn-primary badge-pill" style="width:80px;">edit</a>
                             <button type="button" class="btn btn-danger badge-pill"style="width:80px;">Delete</button>
