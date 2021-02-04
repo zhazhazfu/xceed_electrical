25a54d1d8a9e14f56a5b83a5d4490f20e1132ab2
james fu <fuzihaoooo@gmail.com>
2021-02-04 20:34:35 +1100

    comment the email in login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -50,7 +50,7 @@
                         </div>
 
                         
-                        <div class="form-group row">
+                        {{-- <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
@@ -62,7 +62,7 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        </div> --}}
 
 
                         <div class="form-group row">
